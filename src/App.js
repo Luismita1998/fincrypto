edb16814139127c32f63765301309da73c381848
luismita1998 <lpalacio@udesa.edu.ar>
2021-09-06 17:39:16 -0300

    Creando componente header

--- src/App.js
+++ src/App.js
@@ -5,18 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <header>
-        <h1>Título/ Nombre de la app</h1>
-        <section>
-            <p>Ordenar ASC/ DESC</p>
-            <i class="fas fa-th"></i>
-            <i class="fas fa-align-justify"></i>
-            <form action="">
-                <input type="text" name="search" id="" placeholder="Search"/>
-                <button type="submit"><i class="fas fa-search"></i></button>
-            </form>
-        </section>
-    </header>
     <main>
         <button type="button">Cargar más tarjetas</button>
         <section class="card-container">
