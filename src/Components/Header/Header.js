e972e0db490ee6c8dc6a5b8362ffbac1a74909d7
luismita1998 <lpalacio@udesa.edu.ar>
2021-09-06 18:00:18 -0300

    Empezando

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
+
 function Header(){
     return(
         <header>
+            <nav className="navbar">
         <h1>Título/ Nombre de la app</h1>
         <section>
             <p>Ordenar ASC/ DESC</p>
-            <i class="fas fa-th"></i>
-            <i class="fas fa-align-justify"></i>
+            <i className="fas fa-th"></i>
+            <i className="fas fa-align-justify"></i>
             <form action="">
                 <input type="text" name="search" id="" placeholder="Search"/>
                 <button type="submit"><i class="fas fa-search"></i></button>
             </form>
         </section>
+        </nav>
     </header>
 
     )
