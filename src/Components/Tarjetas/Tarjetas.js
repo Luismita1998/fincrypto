f8dfcbc9f4cd7606781537fbdbac42be39cedc69
Enrico <enrico.9999@hotmail.com>
2021-09-09 12:13:52 -0300

    Colores porcentajes

--- src/Components/Tarjetas/Tarjetas.js
+++ src/Components/Tarjetas/Tarjetas.js
@@ -25,17 +25,32 @@ class Tarjetas extends Component{
     }
 
     render(){
-        return(
-        <div className= 'character-card'>
-            <img src={this.props.dataCrypto.image} alt="" />
-            <h4>{this.props.dataCrypto.name}</h4>
-            <h4 className="color-nombres" >{this.props.dataCrypto.current_price} US$</h4>
-            <h4 className="color-nombres" >{this.props.dataCrypto.price_change_percentage_24h.toFixed(2)} %</h4>
-            <button className="btn btn-outline-danger" onClick = { () => this.props.borrar(this.props.dataCrypto.id)} >Delete</button>
-            <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Total supply: {this.props.dataCrypto.total_supply} All time High: {this.props.dataCrypto.ath} US$</p>
-            <p className='more' className="btn btn-success" onClick={()=>this.viewMore()}>{this.state.text}</p>
-        </div>
-        );
+        if(this.props.dataCrypto.price_change_percentage_24h.toFixed(2)> 0){
+            return(
+                <div className= 'character-card'>
+                    <img src={this.props.dataCrypto.image} alt="" />
+                    <h4>{this.props.dataCrypto.name}</h4>
+                    <h4 className="color-nombres" >{this.props.dataCrypto.current_price} US$</h4>
+                    <h4 className="color-nombres verde" >{this.props.dataCrypto.price_change_percentage_24h.toFixed(2)} %</h4>
+                    <button className="btn btn-outline-danger" onClick = { () => this.props.borrar(this.props.dataCrypto.id)} >Delete</button>
+                    <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Total supply: {this.props.dataCrypto.total_supply} All time High: {this.props.dataCrypto.ath} US$</p>
+                    <p className='more' className="btn btn-success" onClick={()=>this.viewMore()}>{this.state.text}</p>
+                </div>
+                );
+        }else{
+            return(
+                <div className= 'character-card'>
+                    <img src={this.props.dataCrypto.image} alt="" />
+                    <h4>{this.props.dataCrypto.name}</h4>
+                    <h4 className="color-nombres" >{this.props.dataCrypto.current_price} US$</h4>
+                    <h4 className="color-nombres rojo" >{this.props.dataCrypto.price_change_percentage_24h.toFixed(2)} %</h4>
+                    <button className="btn btn-outline-danger" onClick = { () => this.props.borrar(this.props.dataCrypto.id)} >Delete</button>
+                    <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Total supply: {this.props.dataCrypto.total_supply} All time High: {this.props.dataCrypto.ath} US$</p>
+                    <p className='more' className="btn btn-success" onClick={()=>this.viewMore()}>{this.state.text}</p>
+                </div>
+                );
+        }
+
         
     }
 }
