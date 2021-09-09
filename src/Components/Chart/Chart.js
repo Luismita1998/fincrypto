6fc6f89cf89db0e083c8c4deeb098e3a3010d6b9
Enrico <enrico.9999@hotmail.com>
2021-09-09 17:11:13 -0300

    Uno mas

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react';
 import  {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip}  from 'recharts';
-const data = [{name: 'A', uv: 54000},{name: 'B', uv: 49800},{name: '47000', uv: 47000, pv: 2400, amt: 2400},{name: '39000', uv: 39000}];
-
-const renderLineChart2 = (
-    <LineChart width={400} height={400} data={data}>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-    </LineChart>
-  );
-
 
 
 class Chart extends Component{
