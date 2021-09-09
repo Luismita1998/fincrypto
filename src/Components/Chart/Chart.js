4b2b233272ef2923f9c5cd507307270fa75ea24a
luismita1998 <lpalacio@udesa.edu.ar>
2021-09-09 17:37:31 -0300

    Editando como Quique

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import  {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip}  from 'recharts';
+import  {LineChart, Line, Legend, CartesianGrid, XAxis, YAxis, Tooltip}  from 'recharts';
 
 
 class Chart extends Component{
@@ -12,11 +12,12 @@ class Chart extends Component{
                 <div className="row card-container">                
                     {
                     <LineChart width={600} height={300} data={this.props.dataVar} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                    <Line type="monotone" dataKey="price" stroke="#8884d8" />
+                    <Line name="Bitcoin for Quique" type="monotone" dataKey="price" stroke="#8884d8" />
                     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                     <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
+                    <Legend verticalAlign="top" height={36}/>
                     </LineChart>
                     }
                 </div>
