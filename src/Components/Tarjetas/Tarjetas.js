183d65f4e9920b8ad53f10a9ae4270a404557eda
Enrico <enrico.9999@hotmail.com>
2021-09-14 22:29:24 -0300

    cambiando if ternario color

--- src/Components/Tarjetas/Tarjetas.js
+++ src/Components/Tarjetas/Tarjetas.js
@@ -11,31 +11,17 @@ class Tarjetas extends Component{
     }
 
     render(){
-        if(this.props.dataCrypto.price_change_percentage_24h.toFixed(2)> 0){
+        let fontColor = this.props.dataCrypto.price_change_percentage_24h.toFixed(2) > 0 ? "color-nombres verde" : "color-nombres rojo"
             return(
                 <div className= 'character-card'>
                     <img src={this.props.dataCrypto.image} alt="" />
                     <h4>{this.props.dataCrypto.name}</h4>
                     <h4 className="color-nombres" >{this.props.dataCrypto.current_price} US$</h4>
-                    <h4 className="color-nombres verde" >{this.props.dataCrypto.price_change_percentage_24h.toFixed(2)} %</h4>
+                    <h4 className={fontColor} >{this.props.dataCrypto.price_change_percentage_24h.toFixed(2)} %</h4>
                     <button id="espacio" className="btn btn-outline-danger" onClick = { () => this.props.borrar(this.props.dataCrypto.id)}>Delete</button>
                     <button className="btn btn-success" name={this.props.dataCrypto.id} onClick = { () => this.props.infoChart(this.props.dataCrypto.id)} >INFO</button>
                 </div>
                 );
-        }else{
-            return(
-                <div className= 'character-card'>
-                    <img src={this.props.dataCrypto.image} alt="" />
-                    <h4>{this.props.dataCrypto.name}</h4>
-                    <h4 className="color-nombres" >{this.props.dataCrypto.current_price} US$</h4>
-                    <h4 className="color-nombres rojo" >{this.props.dataCrypto.price_change_percentage_24h.toFixed(2)} %</h4>
-                    <button id="espacio" className="btn btn-outline-danger" onClick = { () => this.props.borrar(this.props.dataCrypto.id)} >Delete</button>
-                    <button className="btn btn-success" name={this.props.dataCrypto.id} onClick = { () => this.props.infoChart(this.props.dataCrypto.id)} >INFO</button>
-                </div>
-                );
-        }
-
-        
     }
 }
 
